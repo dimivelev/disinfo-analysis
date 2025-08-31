ef769d82ca40a4b25ed3df3c224744d07cd39639
deadmitko <deadmitko@gmail.com>
2025-08-31 15:32:16 +0300

    hui4

--- main.py
+++ main.py
@@ -101,6 +101,7 @@ async def analyze_html_streaming(payload: HtmlPayload):
 async def read_root():
     return {"message": "Welcome to the Disinformation Analysis API. Visit /docs to see the API documentation."}
 
+port = int(os.getenv("PORT", 8000))
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
